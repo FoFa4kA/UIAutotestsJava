f099de7d98cc2188fb28d010c811af038add36b9
Vladimir Askerov <vovanask@rambler.ru>
2024-11-28 19:27:10 +0300

    code changes

--- src/test/java/util/TestListener.java
+++ src/test/java/util/TestListener.java
@@ -1,19 +1,13 @@
 package util;
 
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.IInvokedMethod;
 import org.testng.IInvokedMethodListener;
 import org.testng.ITestResult;
-import ru.yandex.qatools.ashot.AShot;
-import ru.yandex.qatools.ashot.Screenshot;
-import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 import tests.base.BaseTest;
-
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-
-import static util.AddAttachment.getBytes;
 
 public class TestListener implements IInvokedMethodListener {
 
@@ -22,15 +16,8 @@ public class TestListener implements IInvokedMethodListener {
         Object currentTestClass = testResult.getInstance();
         WebDriver driver = ((BaseTest) currentTestClass).getDriver();
         if (testResult.getStatus() == ITestResult.FAILURE) {
-            Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100))
-                    .takeScreenshot(driver);
-            try {
-                ImageIO.write(screenshot.getImage(), "PNG",
-                        new File("src/test/resources/screen.png"));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            getBytes("screen.png");
+            Allure.getLifecycle().addAttachment("screenshot", "image/png", "png",
+                    ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
         }
         IInvokedMethodListener.super.afterInvocation(method, testResult);
     }
