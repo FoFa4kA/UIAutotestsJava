61761b760a8ff86580d8425379d16b53b5a311e1
Vladimir Askerov <vovanask@rambler.ru>
2024-12-02 23:12:22 +0300

    add parallel tests run and grid

--- src/test/java/common/CommonActions.java
+++ src/test/java/common/CommonActions.java
@@ -1,8 +1,12 @@
 package common;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import static util.PropertiesUtil.getProp;
@@ -10,9 +14,15 @@ import static util.PropertiesUtil.getProp;
 public class CommonActions {
 
     public static WebDriver createDriver() {
-        System.setProperty("webdriver.chrome.driver", getProp("chromedriver"));
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Long.parseLong(getProp("implicit_wait"))));
-        return driver;
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setPlatform(Platform.WIN10);
+            capabilities.setBrowserName("chrome");
+            WebDriver driver = new RemoteWebDriver(new URL(getProp("grid_hub")), capabilities);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Long.parseLong(getProp("implicit_wait"))));
+            return driver;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
