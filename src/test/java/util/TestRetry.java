b9ec516781becda03efd7bba2943772186509fb1
Vladimir Askerov <vovanask@rambler.ru>
2024-12-06 22:03:47 +0300

    code changes after review

--- src/test/java/util/TestRetry.java
+++ src/test/java/util/TestRetry.java
@@ -3,17 +3,14 @@ package util;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
+import static util.PropertiesUtil.getProp;
+
 public class TestRetry implements IRetryAnalyzer {
     private int count = 0;
-    private int retryCount = 2;
+    private final int retryCount = Integer.parseInt(getProp("failed_test_retries"));
 
+    @Override
     public boolean retry(ITestResult result) {
-        if (!result.isSuccess()) {
-            if (count < retryCount) {
-                count++;
-                return true;
-            }
-        }
-        return false;
+        return !result.isSuccess() && count++ < retryCount;
     }
 }
